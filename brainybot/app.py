7e816ef22c9a2c5fcd5020be6f69e59b32ab868a
Mimansha Kaur <134724715+MimanshaKaur@users.noreply.github.com>
2025-04-13 18:44:46 +0000

    backend

--- brainybot/app.py
+++ brainybot/app.py
@@ -1,4 +1,10 @@
-import nltk
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('stopwords')
+from flask import Flask, request, jsonify, send_file, render_template
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import os, fitz, whisper
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
+from textblob import TextBlob
+
